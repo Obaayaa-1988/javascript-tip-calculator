506213918a9d5e863c6394a55a60f0be0b0e2277
Obaayaa-1988 <aquaicoe31@gmail.com>
2022-06-11 13:54:39 +0100

    added all script

--- script.js
+++ script.js
@@ -2,14 +2,39 @@ const btn = document.querySelector(".btn");
 const tip = document.querySelector(".tip");
 const total = document.querySelector(".total");
 const error = document.querySelector(".error");
+// a function to hadle the errors when a user doesn't type a number in the bill input field and disappear after 5secs
+const handleErrors = () => {
+    setTimeout(() => {
+        error.style.display = "none";
 
-
-const calculateTips = () => {
-    const bill = document.querySelector(".bill").value
-    const rate = document.querySelector(".rate").value
-    if(bill === "" || rate == ""){
-        console.log("please add an amount of number")
-    }
+    }, 5000)
 }
 
-btn.addEventListener("click", calculateTips)
+//if statement to check when the input field is empty and the user clicks on the calculate button
+//else if statement to check if a user types in a number in the bill input field instead of a letter
+
+const calculateTip = () => {
+    const bill = document.querySelector(".bill").value;
+    const rate = document.querySelector(".rate").value;
+    if(bill == "" || rate == ""){
+        error.style.display = "block";
+        handleErrors();
+
+    }else if (isNaN(bill)) {
+        error.innerHTML = "Please enter a number";
+        error.style.display = "block";
+        handleErrors()
+    } else {
+        let tipAmount = bill + rate;
+        tipAmount = Math.ceil(tipAmount)
+        tip.innerHTML = `Tip: $${tipAmount}`
+
+
+        let totalBill = Number(bill) + tipAmount
+        total.innerHTML = `Total Bill: $${totalBill}`
+        
+    }
+    
+}
+
+btn.addEventListener("click", calculateTip)
